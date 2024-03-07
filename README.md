209256e083eaddca35980a158e50f0697a32c7da
Felix <andrewchris99@yahoo.com>
2024-03-07 18:25:24 -0500

    complete lab

---
+++ README.md
@@ -0,0 +1,1 @@
+# Carmen-Satiago-Deleverable
--- clues.sql
+++ clues.sql
@@ -3,6 +3,7 @@
 -- so find the least populated country in Southern Europe, and we'll start looking for her there.
  
 -- Write SQL query here
+SELECT name, population FROM country ORDER BY population LIMIT 1;
 
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in
@@ -10,14 +11,14 @@
 -- spoken in this country, so we can call in a translator to work with you.
 
 -- Write SQL query here
-
+SELECT * FROM countrylanguage WHERE countrycode LIKE 'VAT%';
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on
 -- to a different country, a country where people speak only the language she was learning. Find out which
 -- nearby country speaks nothing but that language.
 
 -- Write SQL query here
-
+SELECT * FROM countrylanguage WHERE language = 'Italian' and  percentage = '100';
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time.
 -- There are only two cities she could be flying to in the country. One is named the same as the country – that
@@ -25,21 +26,23 @@
 -- be flying to.
 
 -- Write SQL query here
-
+SELECT * FROM city WHERE countrycode LIKE 'SMR';
+3170 | Serravalle | SMR         | Serravalle/Dogano |       4802
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different
 -- parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were
 -- headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
 -- Write SQL query here
-
+SELECT * FROM city WHERE name LIKE 'Serr%';
+SELECT * FROM country WHERE country.code = 'BRA';
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
 -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
 -- follow right behind you!
 
 -- Write SQL query here
-
+SELECT * FROM city WHERE id = 211;
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to
 -- the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the
@@ -56,4 +59,4 @@
 
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
-
+SELECT * FROM city WHERE population = 91084;
